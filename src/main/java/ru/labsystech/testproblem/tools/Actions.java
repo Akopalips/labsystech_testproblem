99bd3ea43f6afc6c1157c897a8e04979a0391f6a
Kondratiev Daniil <daniil.kondratev@softwarecom.ru>
2024-01-09 15:03:06 +0300

    готово

--- src/main/java/ru/labsystech/testproblem/tools/Actions.java
+++ src/main/java/ru/labsystech/testproblem/tools/Actions.java
@@ -12,19 +12,55 @@ import ru.labsystech.testproblem.tools.impl.TwoInOneNoSpaceSerializer;
  * @author tuspring
  */
 public class Actions {
-    public static void main(String... shit) {
-        Actions actions = new Actions();
-//        Serializer serializer = new EazySerializer();
         Serializer serializer = new TwoInOneNoSpaceSerializer();
-//        Deserializer deserializer = new EazyDeserializer();
         Deserializer deserializer = new TwoInOneNoSpaceDeserializer();
+    public static void main(String... cmd) {
+        Actions actions = new Actions();
 
-        int minValue = 1;
-        int maxValue = 300;
-        int length = 1000;
-        List<Integer> rawCollection = actions.createRandomCollection(minValue, maxValue, length);
-//        List<Integer> rawCollection = new ArrayList<>();
-//        rawCollection.add(149);
+        System.out.println("Test with min %s max %s length %s: ".formatted(1, 300, 50)
+                               + ((actions.commonTest(createRandomCollection(1, 300, 50))) ? "success" : "failed"));
+        System.out.println("Test with min %s max %s length %s: ".formatted(1, 300, 100)
+                               + ((actions.commonTest(createRandomCollection(1, 300, 100))) ? "success" : "failed"));
+        System.out.println("Test with min %s max %s length %s: ".formatted(1, 300, 500)
+                               + ((actions.commonTest(createRandomCollection(1, 300, 500))) ? "success" : "failed"));
+        System.out.println("Test with min %s max %s length %s: ".formatted(1, 300, 1000)
+                               + ((actions.commonTest(createRandomCollection(1, 300, 1000))) ? "success" : "failed"));
+
+        System.out.println("Test with min %s max %s length %s: ".formatted(1, 9, 1000)
+                               + ((actions.commonTest(createRandomCollection(1, 9, 1000))) ? "success" : "failed"));
+        System.out.println("Test with min %s max %s length %s: ".formatted(1, 99, 1000)
+                               + ((actions.commonTest(createRandomCollection(1, 99, 1000))) ? "success" : "failed"));
+        System.out.println("Test with min %s max %s length %s: ".formatted(1, 300, 1000)
+                               + ((actions.commonTest(createRandomCollection(1, 300, 1000))) ? "success" : "failed"));
+
+        List<Integer> customCollection = new ArrayList<>();
+        for (int i = 1; i <= 3 ; i++) {
+            for (int j = 1; j <= 300; j++) {
+                customCollection.add(j);
+            }
+        }
+        System.out.println("Test with min %s max %s length %s with triple number repeat: ".formatted(1, 300, 900)
+                               + ((actions.commonTest(customCollection)) ? "success" : "failed"));
+        if(cmd.length>2) {
+            System.out.println("Test with min %s max %s length %s: ".formatted(cmd[0], cmd[1], cmd[2])
+                                   + ((actions.detailedTest(createRandomCollection(
+                Integer.parseInt(cmd[0]), Integer.parseInt(cmd[1]), Integer.parseInt(cmd[2])))) ? "success"
+                : "failed"));
+        }
+    }
+
+    private boolean commonTest(List<Integer> rawCollection){
+        int rawCollectionLength = integerCollectionLength(rawCollection);
+        System.out.println("Collection of integers length: " + rawCollectionLength);
+        String serializedCollection = serializer.serialize(rawCollection);
+        System.out.println("Serialized collection length: " + serializedCollection.length());
+        System.out.println("Compressed by " + (serializedCollection.length()*100 / rawCollectionLength) + "%.");
+        List<Integer> deserializedString = deserializer.deserialize(serializedCollection);
+        System.out.println("Deserialized string length: " + integerCollectionLength(deserializedString));
+        return rawCollection.equals(deserializedString);
+    }
+
+    private boolean detailedTest(List<Integer> rawCollection){
         int rawCollectionLength = integerCollectionLength(rawCollection);
         System.out.println("Collection of integers: " + rawCollection + " with length " + rawCollectionLength);
         String serializedCollection = serializer.serialize(rawCollection);
@@ -32,7 +68,7 @@ public class Actions {
         System.out.println("Compressed " + (serializedCollection.length()*100 / rawCollectionLength) + "%.");
         List<Integer> deserializedString = deserializer.deserialize(serializedCollection);
         System.out.println("Deserialized string: " + deserializedString + " with length " + integerCollectionLength(deserializedString));
-        System.out.println(rawCollection.equals(deserializedString) ? "Success!!" : "Ooops..");
+        return rawCollection.equals(deserializedString);
     }
 
     private static int integerCollectionLength(List<Integer> testCollection) {
@@ -43,7 +79,7 @@ public class Actions {
         return --rawCollectionLength;
     }
 
-    private List<Integer> createRandomCollection(int minValue, int maxValue, int length){
+    private static List<Integer> createRandomCollection(int minValue, int maxValue, int length){
         ArrayList<Integer> result = new ArrayList<>();
         Random random = new Random();
         for (int i = 0; i < length; i++){
